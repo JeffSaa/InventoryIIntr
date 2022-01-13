760c545f6cc26d158b18b46f3e795b8d80cc83dc
Jeff <jeffreysaa@outlook.com>
2022-01-13 08:56:54 -0500

    Deleted column added to Item model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_13_132728) do
+ActiveRecord::Schema.define(version: 2022_01_13_135409) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2022_01_13_132728) do
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "deleted", default: false
   end
 
 end
