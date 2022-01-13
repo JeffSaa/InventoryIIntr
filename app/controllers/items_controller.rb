aa3093d679d818051299377badcb199e08dbfccb
Jeff <jeffreysaa@outlook.com>
2022-01-13 18:47:59 -0500

    Create item method

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
 
   def index
-  	@items = Item.all.where(deleted: false)
+  	@items = Item.all.where(deleted: false).order('created_at DESC')
   end
 
   def destroy
@@ -9,6 +9,25 @@ class ItemsController < ApplicationController
   	if item.update(deleted: true)
   		redirect_to items_path
   	end
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    item = Item.new(item_params)
+    if item.save
+      redirect_to new_item_url
+    else
+      redirect_to new_item_url, notice: item.errors.full_messages
+    end
+  end
+
+  private
+
+  def item_params
+    params[:item].permit(:name, :description, :quantity)
   end
 
 end
