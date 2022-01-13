3b67317d56c4b280ea3512b6c008d0130747bb20
Jeff <jeffreysaa@outlook.com>
2022-01-13 08:53:34 -0500

    Item table basic design

--- db/migrate/20220113132728_create_items.rb
+++ db/migrate/20220113132728_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
       t.string :name
-      t.integer :quantity
+      t.integer :quantity, default: 0
       t.string :description
 
       t.timestamps
