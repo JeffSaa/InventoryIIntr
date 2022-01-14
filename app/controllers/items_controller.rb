caeebeede0f73379a70b4901de3ad765c943cf56
Jeff <jeffreysaa@outlook.com>
2022-01-14 09:41:38 -0500

    Update item method

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,11 +4,8 @@ class ItemsController < ApplicationController
   	@items = Item.all.where(deleted: false).order('created_at DESC')
   end
 
-  def destroy
-  	item = Item.find(params[:id])
-  	if item.update(deleted: true)
-  		redirect_to items_path
-  	end
+  def show
+    @item = Item.find(params[:id])
   end
 
   def new
@@ -18,9 +15,25 @@ class ItemsController < ApplicationController
   def create
     item = Item.new(item_params)
     if item.save
-      redirect_to new_item_url
+      redirect_to new_item_url, notice: ["Item Created"]
     else
       redirect_to new_item_url, notice: item.errors.full_messages
+    end
+  end
+
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to item_url, notice: ["Item Updated"]
+    else
+      redirect_to item_url, notice: item.errors.full_messages
+    end
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    if item.update(deleted: true)
+      redirect_to items_path
     end
   end
 
